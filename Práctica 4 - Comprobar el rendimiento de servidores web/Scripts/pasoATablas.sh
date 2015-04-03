f1934e1931c125e65e7adcbfe3365b33c474f7e6
analca3 <analca3@gmail.com>
2015-04-03 17:57:22 +0200

    Terminado script para pasar a tablas: Ahora uso HTML para tener algo más de potencia

--- Práctica 4 - Comprobar el rendimiento de servidores web/Scripts/pasoATablas.sh
+++ Práctica 4 - Comprobar el rendimiento de servidores web/Scripts/pasoATablas.sh
@@ -1,19 +1,63 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
+if [ $# -ne 5 ]
 then
-  echo "Tienes que meter 3 parámetros"
+  echo "Tienes que meter 5 parámetros"
+  echo "./pasoATablas.sh <programa> <parámetro> <nombreTabla> <etiquetaX> <etiquetaY>"
   exit 1
 fi
 
-INPUT="$1"
-XLABEL="$2"
-YLABEL="$3"
+declare -A MAPHOSTS=( ["servidor"]="Servidor web" ["granja_nginx"]="Granja Nginx" ["granja_haproxy"]="Granja Haproxy" )
+HOSTS=(${!MAPHOSTS[@]})
+HOSTS=(${HOSTS[@]/"servidor"})
 
-echo "| $XLABEL | $YLABEL |  Error  |"
-echo "| :-----: | :-----: | :-----: |"
+PROGRAMA="$1"
+PARAMETRO="$2"
+NOMBRETABLA="$3"
+XLABEL="$4"
+YLABEL="$5"
 
-while read dato media error
+tmp=$( mktemp )
+tmp2=$( mktemp )
+tmp3=$( mktemp )
+
+cat "../Datos/$PROGRAMA-${HOSTS[0]}-$PARAMETRO.dat" | cut -d" " -f2- > $tmp
+cat "../Datos/$PROGRAMA-${HOSTS[1]}-$PARAMETRO.dat" | cut -d" " -f2- > $tmp2
+
+paste -d" " "../Datos/$PROGRAMA-servidor-$PARAMETRO.dat" $tmp $tmp2 > $tmp3
+
+echo "<table>"
+echo "  <tr>"
+echo "    <th colspan="7" style="text-align:center">$NOMBRETABLA</th>"
+echo "  </tr>"
+echo "  <tr>"
+echo "    <th></th>"
+echo "    <th colspan="2" style="text-align:center">${MAPHOSTS["servidor"]}</th>"
+echo "    <th colspan="2" style="text-align:center">${MAPHOSTS[${HOSTS[0]}]}</th>"
+echo "    <th colspan="2" style="text-align:center">${MAPHOSTS[${HOSTS[1]}]}</th>"
+echo "  </tr>"
+echo "  <tr>"
+echo "    <th>Peticiones</th>"
+echo "    <th>Media</th>"
+echo "    <th>Error</th>"
+echo "    <th>Media</th>"
+echo "    <th>Error</th>"
+echo "    <th>Media</th>"
+echo "    <th>Error</th>"
+echo "   </tr>"
+
+while read -r peticiones media1 error1 media2 error2 media3 error3
 do
-  echo "| $dato | $media | $error |"
-done < $INPUT
+  echo "  <tr>"
+  echo "    <td>$peticiones"
+  echo "    <td>$media1"
+  echo "    <td>$error1"
+  echo "    <td>$media2"
+  echo "    <td>$error2"
+  echo "    <td>$media3"
+  echo "    <td>$error3"
+  echo "  </tr>"
+done < $tmp3
+echo "</table>"
+
+echo "![Gráfica $PROGRAMA-$PARAMETRO](Imágenes/$PROGRAMA-$PARAMETRO.png)"
